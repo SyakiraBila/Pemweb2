8ecc402e7607bee0d7b64e7ad9b83557cc0f6575
Syakira <alexazylviander@gmail.com>
2024-03-20 21:49:09 +0700

    tugas dan praktek pekan4

---
+++ praktik5/class_lingkaran.php
@@ -0,0 +1,29 @@
+<?php
+class Lingkaran{
+   /**
+    * Access Modifier:
+    * 1. public: properti/method dapat diakses dimanapun
+    * 2. private: properti/method hanya dapat diakses didalam cllas tsb.
+    * 3. protected: mirip seperti private, tetapi dapat diakses juga
+    * oleh child class
+    */
+
+    //properti
+    private $jari;
+    const PHI = 3.14;
+
+    //methode: function yang berada di dalam cllas
+    public function __construct($r)
+    {
+        $this->jari = $r;
+    }
+
+    public function getLuas(){
+        return self::PHI * $this->jari * $this->jari;
+    }
+
+    public function getKeliling () {
+        return 2 * self::PHI * $this->jari;
+    }
+}
+
---
+++ praktik5/data_lingkaran.php
@@ -0,0 +1,15 @@
+<?php
+require_once('class_lingkaran.php');
+echo "Nilai PHI: " . Lingkaran::PHI;
+
+// instansiasi object Lingkaran
+$lingkar1 = new Lingkaran(10);
+$lingkar2 = new Lingkaran(8);
+
+// Panggil method
+echo "<br> Luas Lingkaran 1: " . $lingkar1->getLuas();
+echo "<br> Luas Lingkaran 2: " . $lingkar2->getLuas(); 
+
+// Panggil method
+echo "<br> Keliling Lingkaran 1: " . $lingkar1->getKeliling();
+echo "<br> Keliling Lingkaran 2: " . $lingkar2->getKeliling(); 
